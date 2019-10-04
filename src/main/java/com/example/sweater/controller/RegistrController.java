a26b772586358d0e0ae0c07187236b8f4653a027
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2019-10-04 13:59:58 +0200

    Add RegistrController

---
+++ src/main/java/com/example/sweater/controller/RegistrController.java
@@ -0,0 +1,40 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+
+@Controller
+public class RegistrController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registr")
+    public String registr(){
+        return "registr";
+    }
+
+    @PostMapping("/registr")
+    public String addUser(User user, Model model){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb != null){
+            model.addAttribute("message","User exests!");
+            return "registr";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+}
