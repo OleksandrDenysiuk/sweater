2cc953b20ddd6fd561afba28490d44b4ba91dcc3
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2019-10-07 06:13:25 +0200

    Add activate method

--- src/main/java/com/example/sweater/controller/RegistrController.java
+++ src/main/java/com/example/sweater/controller/RegistrController.java
@@ -3,10 +3,12 @@ package com.example.sweater.controller;
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
@@ -15,7 +17,7 @@ import java.util.Collections;
 public class RegistrController {
 
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registr")
     public String registr(){
@@ -24,17 +26,23 @@ public class RegistrController {
 
     @PostMapping("/registr")
     public String addUser(User user, Model model){
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-
-        if(userFromDb != null){
-            model.addAttribute("message","User exests!");
+        if (!userService.addUser(user)) {
+            model.addAttribute("message", "User exists!");
             return "registr";
         }
-
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-
-        userRepo.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
     }
 }
