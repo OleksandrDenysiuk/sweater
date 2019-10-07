c237d8d76e1ed1a11000369b6dacd6b7041dbf68
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2019-10-07 06:10:11 +0200

    Add mapping "activate/*"

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registr", "static/**").permitAll()
+                .antMatchers("/", "/registr", "static/**","activate/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
