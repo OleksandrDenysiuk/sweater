83fd2ea626fc861fff62252f1c6c0593f7f8c96b
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2019-10-08 06:14:20 +0200

    Add validation to form

--- src/main/java/com/example/sweater/controller/RegistrController.java
+++ src/main/java/com/example/sweater/controller/RegistrController.java
@@ -7,11 +7,14 @@ import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Collections;
+import java.util.Map;
 
 @Controller
 public class RegistrController {
@@ -25,7 +28,19 @@ public class RegistrController {
     }
 
     @PostMapping("/registr")
-    public String addUser(User user, Model model){
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
+        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+            model.addAttribute("passwordError", "Passwords are different!");
+        }
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+
+            model.mergeAttributes(errors);
+
+            return "registr";
+        }
+
         if (!userService.addUser(user)) {
             model.addAttribute("message", "User exists!");
             return "registr";
